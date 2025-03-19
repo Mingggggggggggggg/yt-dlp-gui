c24a9b3c4951c44c7d3f9bd6c4c89da2331c564b
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-03-19 12:18:32 +0100

    Update settings.py

---
+++ settings.py
@@ -0,0 +1,3 @@
+# Einstellungen als Json im selben ordner speichern und laden
+# So sollen benutzerdefinierte Standardeinstellungen eingestellt werden können
+# Existiert die Datei nicht, wird diese erstellt, sonst lade aus der Datei
